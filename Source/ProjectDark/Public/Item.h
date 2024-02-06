d00fc8f2cd659f739338de052173e9f42a7f573f
Craig <kaireigu@gmail.com>
2024-02-06 16:25:27 +0000

    Added Item Class & BP. Setup Overlap Functionality on Item Class.

---
+++ Source/ProjectDark/Public/Item.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+class UStaticMeshComponent;
+class USphereComponent;
+
+UCLASS()
+class PROJECTDARK_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(EditAnywhere)
+	USphereComponent* SphereComponent;
+
+private:
+
+	UFUNCTION()
+	void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
